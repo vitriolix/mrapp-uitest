1d4c88709d4ecb14d10a962b5baa9cadb54f24a6
Josh Steiner <josh@vitriolix.com>
2012-10-15 22:19:28 -0700

    adding cliptype icons; added layouts for Lessons and NewStory; hooked up basic navigation

---
+++ src/info/guardianproject/misterapp/NewStory.java
@@ -0,0 +1,36 @@
+package info.guardianproject.misterapp;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+
+public class NewStory extends SherlockActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_story);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.activity_new_story, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
