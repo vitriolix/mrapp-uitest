b3cb9f3ebbed0e57a0d05ca41baf13e3e4902e41
Josh Steiner <josh@vitriolix.com>
2012-10-18 20:58:47 -0700

    viewpage for clip icons now loads; change SceneEditor to be non-swipe tabs

--- src/info/guardianproject/mrappuitest/SceneEditorNoSwipe.java
+++ src/info/guardianproject/mrappuitest/SceneEditorNoSwipe.java
@@ -3,9 +3,8 @@ package info.guardianproject.mrappuitest;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
 
-import android.content.Intent;
+import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
@@ -20,84 +19,50 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class SceneEditor extends SherlockFragmentActivity implements ActionBar.TabListener {
+public class SceneEditorNoSwipe extends SherlockFragmentActivity implements ActionBar.TabListener {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
-     * sections. We use a {@link android.support.v4.app.FragmentPagerAdapter} derivative, which will
-     * keep every loaded fragment in memory. If this becomes too memory intensive, it may be best
-     * to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scene_editor);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // Create the adapter that will return a fragment for each of the three primary sections
-        // of the app.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        setContentView(R.layout.activity_scene_editor_no_swipe);
 
         // Set up the action bar.
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.requestDisallowInterceptTouchEvent(true);
-        
-        // When swiping between different sections, select the corresponding tab.
-        // We can also use ActionBar.Tab#select() to do this if we have a reference to the
-        // Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-
         // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by the adapter.
-            // Also specify this Activity object, which implements the TabListener interface, as the
-            // listener for when this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(R.string.tab_add_clips).setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(R.string.tab_order).setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(R.string.tab_publish).setTabListener(this));
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+            getSupportActionBar().setSelectedNavigationItem(
+                    savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM,
+                getSupportActionBar().getSelectedNavigationIndex());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getSupportMenuInflater().inflate(R.menu.activity_scene_editor, menu);
+        getSupportMenuInflater().inflate(R.menu.activity_scene_editor_no_swipe, menu);
         return true;
     }
 
     
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
@@ -105,8 +70,20 @@ public class SceneEditor extends SherlockFragmentActivity implements ActionBar.T
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
+        // When the given tab is selected, show the tab contents in the container
+        int layout = R.layout.fragment_add_clips;
+        if (tab.getPosition() == 1) {
+            layout = R.layout.fragment_order_clips;
+        } else if (tab.getPosition() == 2) {
+            layout = R.layout.fragment_story_publish;
+        } 
+        Fragment fragment = new DummySectionFragment(layout, getSupportFragmentManager());
+        Bundle args = new Bundle();
+        args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, tab.getPosition() + 1);
+        fragment.setArguments(args);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 
     @Override
@@ -114,56 +91,10 @@ public class SceneEditor extends SherlockFragmentActivity implements ActionBar.T
     }
 
     /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
-     * sections of the app.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int i) {
-            int layout = R.layout.fragment_add_clips;
-            if (i == 1) {
-                layout = R.layout.fragment_order_clips;
-            } else if (i == 2) {
-                layout = R.layout.fragment_story_publish;
-            } 
-            Fragment fragment = new DummySectionFragment(layout, getSupportFragmentManager());
-            Bundle args = new Bundle();
-            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
-            fragment.setArguments(args);
-            
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0: return getString(R.string.tab_add_clips).toUpperCase();
-                case 1: return getString(R.string.tab_order).toUpperCase();
-                case 2: return getString(R.string.tab_publish).toUpperCase();
-            }
-            return null;
-        }
-    }
-
-    /**
      * A dummy fragment representing a section of the app, but that simply displays dummy text.
      */
     public static class DummySectionFragment extends Fragment {
         int layout;
-        
-        /**
-         * The {@link mClipViewPager} that will host the clips.
-         */
         ViewPager mClipViewPager;
         ClipPagerAdapter mClipPagerAdapter;
         
@@ -201,7 +132,7 @@ public class SceneEditor extends SherlockFragmentActivity implements ActionBar.T
             }
             return view;
         }
-
+        
         /**
          * A {@link FragmentPagerAdapter} that returns a fragment corresponding to the clips we are editing
          */
@@ -226,7 +157,7 @@ public class SceneEditor extends SherlockFragmentActivity implements ActionBar.T
             }
         }
     }
-
+    
     /**
      * A dummy fragment representing a section of the app, but that simply displays dummy text.
      */
--- src/info/guardianproject/mrappuitest/StoryNew.java
+++ src/info/guardianproject/mrappuitest/StoryNew.java
@@ -23,7 +23,7 @@ public class StoryNew extends SherlockActivity {
             
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getBaseContext(), SceneEditor.class));
+                startActivity(new Intent(getBaseContext(), SceneEditorNoSwipe.class));
             }
         });
         
